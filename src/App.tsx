1bec016dbc6f88479bd75dd9b5e2f1d81c248dd5
hizikata <macair0528@gmail.com>
2023-10-31 22:53:00 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  //returnの中身はHTMLと同じような動きをする
+  //JSXと呼ばれる
+  return (
+    <div className="app">
+      <h1>Hello,world!</h1>
+
+
+    </div>
+  );
+}
+
+export default App;
