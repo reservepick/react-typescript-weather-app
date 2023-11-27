cb6d5bb4a09ca58a18b457c8e00f3d009442d42a
hizikata <macair0528@gmail.com>
2023-11-27 22:48:27 +0900

    76ページまで

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,14 @@
 //下記形がreactのベースとなる
 //inputは最後にスラッシュ
+//[city,setCity]→cityがstate、setCityで操作する、オブジェクト的なやつ?
+//[]の二つ目は一つ目の単語(state名)の前にset加えた形が理想(set+state名)
+import {useState} from 'react'; 
 const Form = () => {
+    const [city,setCity] = useState<string>("");
     return(
         <form>
-            <input type = "text" name = "city" placeholder="都市名"/>
+            <input type = "text" name = "city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
+            {city}
             <button type = "submit">Get Weather</button>
         </form>
     );
