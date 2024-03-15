a5e6b6a72aadf38880d19eb679ef64e28eba2881
reservepick <macair0528@gmail.com>
2024-03-15 17:30:05 +0900

    加筆

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,29 @@ import './App.css';
 //作成したTitleとFormのcomponentをimport
 import Title from './components/Title';
 import Form from './components/Form';
-
-//new pc test
+import Results from './components/Results';
+import {useState} from 'react'; 
 
 function App() {
+  const [city,setCity] = useState<string>("");
+  //都市名のでーたをAOIに渡す処理
+  //console.log(見たいデータの変数)=ブラウザの開発者ツールのコンソールにログを出力するデバックで使いそうな機能
+  //preventDefault() => 画面の更新を止めるめそっど
+  const getWeather = (e:any) =>{
+      e.preventDefault();
+      fetch("http://api.weatherapi.com/v1/current.json?key=b44e5b93b92146efa9772500240903&q=London&aqi=no") 
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
   //returnの中身はHTMLと同じような動きをする
   //JSXと呼ばれる
+  //値を渡す場合はApp(上位のコンポーネント)を経由しなければいけない
+  //一つのコンポーネントが一元管理をすることで管理を容易にするため
   return (
     <div className="app">
       <Title />
-      <Form />
-
+      <Form setCity={setCity} getWeather={getWeather}/>
+      <Results/>
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,22 +2,16 @@
 //inputは最後にスラッシュ
 //[city,setCity]→cityがstate、setCityで操作する、オブジェクト的なやつ?
 //[]の二つ目は一つ目の単語(state名)の前にset加えた形が理想(set+state名)
-import {useState} from 'react'; 
-const Form = () => {
-    const [city,setCity] = useState<string>("");
-    //都市名のでーたをAOIに渡す処理
-    //console.log(見たいデータの変数)=ブラウザの開発者ツールのコンソールにログを出力するデバックで使いそうな機能
-    //preventDefault() => 画面の更新を止めるめそっど
-    const getWeather = (e:any) =>{
-        e.preventDefault();
-        fetch("http://api.weatherapi.com/v1/current.json?key=b44e5b93b92146efa9772500240903&q=London&aqi=no") 
-        .then(res => res.json())
-        .then(data => console.log(data))
-    }
+type FormPropsType={
+    setCity:React.Dispatch<React.SetStateAction<string>>
+    getWeather:(e: any) => void
+}
+
+const Form = (props:FormPropsType) => {
     return(
         <form>
-            <input type = "text" name = "city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
-            <button type = "submit" onClick={getWeather}>Get Weather</button>
+            <input type = "text" name = "city" placeholder="都市名" onChange={e => props.setCity(e.target.value)}/>
+            <button type = "submit" onClick={props.getWeather}>Get Weather</button>
         </form>
     );
 };
